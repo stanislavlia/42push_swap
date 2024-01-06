8c487ac88bc6866bd6a2e8522ffa36e191fcc7a6
Stanislav Liashkov <sliashko@z1t3c4.42bangkok.com>
2024-01-06 12:34:44 +0700

    I turned on alg for sorting 5 elements

--- main.c
+++ main.c
@@ -1,16 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sliashko <sliashko@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/03 14:53:46 by sliashko          #+#    #+#             */
-/*   Updated: 2024/01/04 17:29:25 by sliashko         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include	"push_swap.h"
+#include "push_swap.h"
 
 void free_stack(t_node **head)
 {
@@ -58,33 +46,33 @@ void	print_stack_backward(t_node **head)
 	}
 	
 }
+//TODO
+// 1) Try to comment my small algorithm and try to catch problem using big algorithm
+// 2) Try Xcode to debug and catch problem
 
-/* TODO
+//./push_swap 4 5 1 3 2 - Element 2 just disappears 
+int	main(int argc, char **argv)
+{
+	t_node	*stack_a;
+	t_node	*stack_b;
 
-	1) Implement function to set target node (for els in B)
-	2) Implement function to compute cost of each el in B
-	3) Implement function to rotate untill we have target on top +
-	   function to move from B to A and keep A sorted
-*/
-
-// //DONT forget to check for sorted or not
-// int	main(int argc, char **argv)
-// {
-// 	t_node	*stack_a;
-// 	t_node	*stack_b;
-
-// 	if (argc < 2)
-// 	{
-// 		printf("Too few args\n");
-// 		return (EXIT_FAILURE);
-// 	}
+	if (argc < 2)
+	{
+		printf("Too few args\n");
+		return (EXIT_FAILURE);
+	}
 	
-// 	stack_a = init_stack(argv + 1);
-// 	stack_b = NULL;
+	stack_a = init_stack(argv + 1);
+	stack_b = NULL;
+	if (is_sorted(&stack_a))
+	{
+		free_stack(&stack_a);
+		free_stack(&stack_b);
+		return (EXIT_SUCCESS);
+	}
+	general_sort(&stack_a, &stack_b);
 	
-// 	general_sort(&stack_a, &stack_b);
-	
-// 	free_stack(&stack_a);
-// 	free_stack(&stack_b);
-// 	return (EXIT_FAILURE);
-// }
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+	return (EXIT_SUCCESS);
+}
--- reverse_rotate_operation.c
+++ reverse_rotate_operation.c
@@ -6,49 +6,11 @@
 /*   By: sliashko <sliashko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 16:12:48 by sliashko          #+#    #+#             */
-/*   Updated: 2024/01/06 12:15:49 by sliashko         ###   ########.fr       */
+/*   Updated: 2024/01/06 12:26:47 by sliashko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-
-
-// void	reverse_rotate_a(t_node **stack_a, int verbose)
-// {
-// 	t_node	*temp_node;
-// 	t_node	*last_node;
-// 	if (*stack_a == NULL || (*stack_a)->next == NULL || get_stack_size(stack_a) == 1)
-// 		return ;
-// 	temp_node = find_last_node(stack_a);
-// 	if (temp_node->prev != NULL)
-// 		temp_node->prev->next = NULL;
-// 	temp_node->next = *stack_a;
-// 	temp_node->prev = NULL;
-// 	(*stack_a)->prev = temp_node;	
-// 	*stack_a = temp_node;
-// 	if (verbose)
-// 		ft_putstr_fd("rra\n", 1);
-// }
-
-
-
-// void	reverse_rotate_b(t_node **stack_b, int verbose)
-// {
-// 	t_node	*temp_node;
-// 	t_node	*last_node;
-// 	if (*stack_b == NULL || (*stack_b)->next == NULL || get_stack_size(stack_b) == 1)
-// 		return ;
-// 	temp_node = find_last_node(stack_b);
-// 	if (temp_node->prev != NULL)
-// 		temp_node->prev->next = NULL;
-// 	temp_node->next = *stack_b;
-// 	temp_node->prev = NULL;
-// 	(*stack_b)->prev = temp_node;	
-// 	*stack_b = temp_node;
-// 	if (verbose)
-// 		ft_putstr_fd("rrb\n", 1);
-// }
 
 void	reverse_rotate_a(t_node **stack_a, int verbose)
 {
@@ -63,7 +25,6 @@ void	reverse_rotate_a(t_node **stack_a, int verbose)
 	last->prev = NULL;
 	*stack_a = last;
 	last->next->prev = last;
-	//print_stack(stack_a);
 	update_prev_refs(stack_a);
 	if (verbose)
 		ft_putstr_fd("rra\n", 1);
